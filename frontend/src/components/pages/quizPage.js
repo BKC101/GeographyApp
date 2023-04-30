d59ebb2905d6e662039cbcf592f6e7d773416172
bchampagneATSSU <b_champagne@salemstate.edu>
2023-04-29 23:30:45 -0400

    Added countries to quiz

--- frontend/src/components/pages/quizPage.js
+++ frontend/src/components/pages/quizPage.js
@@ -1,17 +1,91 @@
 import React, { useState, useEffect } from "react";
 import { Container, Row, Col, Form, Button, Modal } from "react-bootstrap";
+import { useParams } from "react-router-dom";
 
 const QuizPage = () => {
+    const { continent } = useParams();
     const [inputValue, setInputValue] = useState("");
     const [correctWords, setCorrectWords] = useState({});
     const [timer, setTimer] = useState(90);
     const [showModal, setShowModal] = useState(false);
 
-    const words = [
-        ["Argentina", "Bolivia", "Brazil", "Chile"],
-        ["Columbia", "Ecuador", "Guyana", "Paraguay"],
-        ["Peru", "Suriname", "Uruguay", "Venezuela"],
-    ];
+    const continentQuizzes = {
+        world: [
+            ["Afghanistan", "Albania", "Algeria", "Andorra", "Angola", "Antigua and Barbuda", "Argentina", "Armenia", "Australia", "Austria", "Azerbaijan"],
+            ["Bahamas", "Bahrain", "Bangladesh", "Barbados", "Belarus", "Belgium", "Belize", "Benin", "Bhutan", "Bolivia", "Bosnia and Herzegovina", "Botswana", "Brazil", "Brunei", "Bulgaria", "Burkina Faso", "Burundi"],
+            ["Cabo Verde", "Cambodia", "Cameroon", "Canada", "Central African Republic", "Chad", "Chile", "China", "Columbia", "Comoros", "Costa Rica", "Cote d'Ivoire", "Croatia", "Cuba", "Cyprus", "Czechia"],
+            ["Democratic Republic of the Congo", "Denmark", "Djibouti", "Dominica", "Dominican Republic", "Ecuador", "Egypt", "El Salvador", "Equatorial Guinea", "Eritrea", "Estonia", "Eswatini", "Ethiopia"],
+            ["Finland", "France", "Gabon", "Gambia", "Georgia", "Germany", "Ghana", "Greece", "Grenada", "Guinea", "Guyana", "Guinea-Bissau", "Hati", "Hungary"],
+            ["Iceland", "India", "Indonesia", "Iran", "Iraq", "Ireland", "Isreal", "Italy", "Jamaica", "Japan", "Jordan", "Kazakhstan", "Kenya", "Kosovo", "Kuwait", "Kyrgyzstan"],
+            ["Laos", "Latvia", "Lebanon", "Lesotho", "Liberia", "Liechtenstein", "Lithuania", "Luxembourg", "Lybia", "Malaysia", "Maldives", "Malta", "Marshall Islands", "Mauritius", "Mexico", "Micronesia", "Moldova", "Monaco", "Mongolia", "Morocco", "Montenegro","Mozambique", "Myanmar"],
+            ["Namibia", "Nauru", "Nepal", "Netherlands", "New Zealand", "Niger", "Nicaragua", "Nigeria", "North Korea", "North Macedonia", "Norway", "Oman", "Pakistan", "Palau", "Palestine", "Panama", "Papua New Guinea", "Paraguay", "Peru", "Philippines", "Poland", "Portugal"],
+            ["Qatar", "Republic of the Congo", "Romania", "Russia", "Rwanda", "Saint Kitts and Nevis", "Saint Lucia", "Saint Vincent and the Grenadines", "Samoa", "Sao Tome and Principe", "Saudi Arabia", "Senegal", "Seychelles", "Serbia", "Sierra Leone", "Singapore", "Slovakia", "Slovenia", "Soloman Islands", "Somalia", "South Africa", "South Korea", "South Sudan", "Spain", "Sri Lanka", "Sudan", "Suriname", "Sweden", "Switzerland", "Syria"],
+            ["Tajikistan", "Tanzania", "Thailand", "Timor-Leste", "Togo", "Tonga", "Trinidad and Tobago", "Tunisia", "Turkiye", "Turkmenistan", "Tuvalu", "Uganda", "Ukraine", "United Kingdom", "United States of America", "Uruguay", "Uzbekistan", "Vanuatu", "Vatican City", "Venezuela", "Vietnam", "Yemen", "Zambia", "Zimbabwe"]
+        ],
+
+        europe: [
+            ["Albania", "Andorra", "Armenia", "Austria", "Azerbaijan"],
+            ["Belarus", "Belgium", "Bosnia and Herzegovina", "Bulgaria"],
+            ["Croatia", "Cyprus", "Czechia", "Denmark", "Estonia"],
+            ["Finland", "France", "Georgia", "Germany", "Greece"],
+            ["Hungary", "Iceland", "Ireland", "Italy", "Kazakhstan", "Kosovo"],
+            ["Latvia", "Liechtenstein", "Lithuania", "Luxembourg"],
+            ["Malta", "Moldova", "Monaco", "Montenegro", "Netherlands", "North Macedonia", "Norway"],
+            ["Poland", "Portugal", "Romania", "Russia"],
+            ["San Marino", "Serbia", "Slovakia", "Slovenia", "Spain", "Sweden", "Switzerland"],
+            ["Turkiye", "Ukraine", "United Kingdom", "Vatican City"]
+        ],
+
+        asia: [
+            ["Afghanistan", "Armenia", "Azerbaijan"],
+            ["Bahrain", "Bangladesh", "Bhutan", "Brunei"],
+            ["Cambodia", "China", "Cyprus", "Egypt", "Georgia"],
+            ["India", "Indonesia", "Iran", "Iraq", "Isreal"],
+            ["Japan", "Jordan", "Kazakhstan", "Kuwait", "Kyrgyzstan"],
+            ["Laos", "Lebanon", "Malaysia", "Maldives", "Mongolia", "Myanmar"],
+            ["Nepal", "North Korea", "Oman", "Pakistan", "Palestine", "Philippines", "Qatar"],
+            ["Russia", "Saudi Arabia", "Singapore", "South Korea", "Sri Lanka", "Syria"],
+            ["Tajikistan", "Thailand", "Timor-Leste", "Turkiye", "Turkmenistan"],
+            ["United Arab Emirates", "Uzbekistan", "Vietnam", "Yemen"]
+
+        ],
+
+        africa: [
+            ["Algeria", "Angola", "Benin", "Botswana", "Burkina Faso", "Burundi"],
+            ["Cabo Verde", "Cameroon", "Central African Republic", "Chad", "Comoros", "Cote d'Ivoire"],
+            ["Democratic Republic of the Congo", "Djibouti", "Egypt", "Equatorial Guinea", "Eritrea", "Eswatini", "Ethiopia"],
+            ["Gabon", "Gambia", "Ghana", "Guinea", "Guinea-Bissau", "Kenya"],
+            ["Lesotho", "Liberia", "Lybia", "Madagascar", "Malawi", "Mali", "Mauritania"],
+            ["Mauritius", "Morocco", "Mozambique", "Namibia", "Niger", "Nigeria", "Republic of the Congo", "Rwanda"],
+            ["Sao Tome and Principe", "Senegal", "Seychelles", "Sierra Leone"],
+            ["Somalia", "South Africa", "South Sudan", "Sudan"],
+            ["Tanzania", "Togo", "Tunisia", "Uganda", "Zambia", "Zimbabwe"]
+        ],
+
+        northAmerica: [
+            ["Antigua and Barbuda", "Bahamas", "Barbados", "Belize"],
+            ["Canada", "Costa Rica", "Cuba", "Dominica", "Dominican Republic"],
+            ["El Salvador", "Grenada", "Guatemala", "Hati", "Honduras"],
+            ["Jamaica", "Mexico", "Nicaragua", "Panama"],
+            ["Saint Kitts and Nevis", "Saint Lucia", "Saint Vincent and the Grenadines"],
+            ["Trinidad and Tobago", "United States of America"]
+            
+        ],
+
+        southAmerica: [
+            ["Argentina", "Bolivia", "Brazil", "Chile"],
+            ["Columbia", "Ecuador", "Guyana", "Paraguay"],
+            ["Peru", "Suriname", "Uruguay", "Venezuela"],
+        ],
+
+        oceania: [
+            ["Australia", "Fiji", "Kiribati", "Marshall Islands", "Micronesia"],
+            ["Nauru", "New Zealand", "Palau", "Papua New Guinea"],
+            ["Samoa", "Soloman Islands", "Tonga", "Tuvalu", "Vanuatu"]
+        ]
+    };
+
+    const words = continentQuizzes[continent];
 
     useEffect(() => {
         if (timer > 0) {
