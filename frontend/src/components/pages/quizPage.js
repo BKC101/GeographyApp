c0e15bd8428599f033dca14402f8477002dea705
bchampagneATSSU <b_champagne@salemstate.edu>
2023-04-30 22:30:00 -0400

    Added capitals to quizPage

--- frontend/src/components/pages/quizPage.js
+++ frontend/src/components/pages/quizPage.js
@@ -6,7 +6,9 @@ const QuizPage = () => {
     const { continent } = useParams();
     const [inputValue, setInputValue] = useState("");
     const [correctCountries, setCorrectCountries] = useState({});
-    const [showModal, setShowModal] = useState(false);
+    const [showModal1, setShowModal1] = useState(true);
+    const [showModal2, setShowModal2] = useState(false);
+    const handleClose = () => setShowModal1(false);
 
     const continentQuizzes = {
         world: {
@@ -19,25 +21,59 @@ const QuizPage = () => {
                 ["China", "Columbia", "Comoros", "Costa Rica", "Cote d'Ivoire", "Croatia", "Cuba"],
                 ["Cyprus", "Czechia", "Democratic Republic of the Congo", "Denmark", "Djibouti", "Dominica", "Dominican Republic"],
                 ["Ecuador", "Egypt", "El Salvador", "Equatorial Guinea", "Eritrea", "Estonia", "Eswatini"],
-                ["Ethiopia", "Finland", "France", "Gabon", "Gambia", "Georgia", "Germany"],
-                ["Ghana", "Greece", "Grenada", "Guinea", "Guyana", "Guinea-Bissau", "Hati"],
-                ["Hungary", "Iceland", "India", "Indonesia", "Iran", "Iraq", "Ireland"],
-                ["Isreal", "Italy", "Jamaica", "Japan", "Jordan", "Kazakhstan", "Kenya"],
-                ["Kosovo", "Kuwait", "Kyrgyzstan", "Laos", "Latvia", "Lebanon", "Lesotho"],
-                ["Liberia", "Liechtenstein", "Lithuania", "Luxembourg", "Lybia", "Malaysia", "Maldives"],
-                ["Malta", "Marshall Islands", "Mauritius", "Mexico", "Micronesia", "Moldova", "Monaco"],
-                ["Mongolia", "Morocco", "Montenegro", "Mozambique", "Myanmar", "Namibia", "Nauru"],
-                ["Nepal", "Netherlands", "New Zealand", "Niger", "Nicaragua", "Nigeria", "North Korea"],
-                ["North Macedonia", "Norway", "Oman", "Pakistan", "Palau", "Palestine", "Panama"],
-                ["Papua New Guinea", "Paraguay", "Peru", "Philippines", "Poland", "Portugal", "Qatar"],
-                ["Republic of the Congo", "Romania", "Russia", "Rwanda", "Saint Kitts and Nevis", "Saint Lucia", "Saint Vincent and the Grenadines"],
-                ["Samoa", "Sao Tome and Principe", "Saudi Arabia", "Senegal", "Seychelles", "Serbia", "Sierra Leone"],
-                ["Singapore", "Slovakia", "Slovenia", "Soloman Islands", "Somalia", "South Africa", "South Korea"],
-                ["South Sudan", "Spain", "Sri Lanka", "Sudan", "Suriname", "Sweden", "Switzerland"],
-                ["Syria", "Tajikistan", "Tanzania", "Thailand", "Timor-Leste", "Togo", "Tonga"],
-                ["Trinidad and Tobago", "Tunisia", "Turkiye", "Turkmenistan", "Tuvalu", "Uganda", "Ukraine"],
-                ["United Kingdom", "United States of America", "Uruguay", "Uzbekistan", "Vanuatu", "Vatican City", "Venezuela"],
-                ["Vietnam", "Yemen", "Zambia", "Zimbabwe"]
+                ["Ethiopia", "Fiji", "Finland", "France", "Gabon", "Gambia", "Georgia"],
+                ["Germany", "Ghana", "Greece", "Grenada", "Guatemala", "Guinea", "Guyana"],
+                ["Guinea-Bissau", "Hati", "Honduras", "Hungary", "Iceland", "India", "Indonesia"],
+                ["Iran", "Iraq", "Ireland", "Isreal", "Italy", "Jamaica", "Japan"],
+                ["Jordan", "Kazakhstan", "Kenya", "Kiribati", "Kosovo", "Kuwait", "Kyrgyzstan"],
+                ["Laos", "Latvia", "Lebanon", "Lesotho", "Liberia", "Liechtenstein", "Lithuania"],
+                ["Luxembourg", "Lybia", "Madagascar", "Malawi", "Malaysia", "Maldives", "Mali"],
+                ["Mauritania", "Malta", "Marshall Islands", "Mauritius", "Mexico", "Micronesia", "Moldova"],
+                ["Monaco", "Mongolia", "Morocco", "Montenegro", "Mozambique", "Myanmar", "Namibia"],
+                ["Nauru", "Nepal", "Netherlands", "New Zealand", "Niger", "Nicaragua", "Nigeria"],
+                ["North Korea", "North Macedonia", "Norway", "Oman", "Pakistan", "Palau", "Palestine"],
+                ["Panama", "Papua New Guinea", "Paraguay", "Peru", "Philippines", "Poland", "Portugal"],
+                ["Qatar", "Republic of the Congo", "Romania", "Russia", "Rwanda", "Saint Kitts and Nevis", "Saint Lucia"],
+                ["Saint Vincent and the Grenadines", "Samoa", "San Marino", "Sao Tome and Principe", "Saudi Arabia", "Senegal", "Seychelles"],
+                ["Serbia", "Sierra Leone", "Singapore", "Slovakia", "Slovenia", "Soloman Islands", "Somalia"],
+                ["South Africa", "South Korea", "South Sudan", "Spain", "Sri Lanka", "Sudan", "Suriname"],
+                ["Sweden", "Switzerland", "Syria", "Taiwan", "Tajikistan", "Tanzania", "Thailand"],
+                ["Timor-Leste", "Togo", "Tonga", "Trinidad and Tobago", "Tunisia", "Turkiye", "Turkmenistan"],
+                ["Tuvalu", "Uganda", "Ukraine", "United Arab Emirates", "United Kingdom", "United States of America", "Uruguay"],
+                ["Uzbekistan", "Vanuatu", "Vatican City", "Venezuela", "Vietnam", "Yemen", "Zambia"],
+                ["Zimbabwe"]
+            ],
+
+            capitals: [
+                ["Abu Dhabi", "Abuja", "Accra", "Addis Ababa", "Algiers", "Amman", "Amsterdam"],
+                ["Andorra la Vella", "Ankara", "Antananarivo", "Apia", "Ashgabat", "Asmara", "Astana"],
+                ["Asuncion", "Athens", "Baghdad", "Baku", "Bamako", "Bandar Seri Begawan", "Bangkok"],
+                ["Bangui", "Banjul", "Basseterre", "Beijing", "Beirut", "Belgrade", "Belmopan"],
+                ["Berlin", "Bern", "Bishkek", "Bissau", "Bogota", "Brasilia", "Bratislava"],
+                ["Brazzaville", "Bridgetown", "Brussels", "Bucharest", "Budapest", "Buenos Aires", "Cairo"],
+                ["Canberra", "Caracas", "Castries", "Chisinau", "Colombo", "Conakry", "Copenhagen"],
+                ["Dakar", "Damascus", "Dhaka", "Dili", "Djibouti", "Dodoma", "Doha"],
+                ["Dublin", "Dushanbe", "Freetown", "Funafuti", "Gaborone", "Georgetown", "Gitega"],
+                ["Guatemala City", "Hanoi", "Harare", "Havana", "Helsinki", "Honiara", "Islamabad"],
+                ["Jakarta", "Jerusalem", "Juba", "Kabul", "Kampala", "Kathmandu", "Khartoum"],
+                ["Kigali", "Kingston", "Kingstown", "Kinshasa", "Kuala Lumpur", "Kuwait City", "Kyiv"],
+                ["Libreville", "Lilongwe", "Lima", "Lisbon", "Ljubljana", "Lome", "London"],
+                ["Luanda", "Lusaka", "Luxembourg", "Madrid", "Majuro", "Malabo", "Male"],
+                ["Managua", "Manama", "Manila", "Maputo", "Maseru", "Mbabane", "Mexico City"],
+                ["Minsk", "Mogadishu", "Monaco", "Monrovia", "Montevideo", "Moroni", "Moscow"],
+                ["Muscat", "N'Djamena", "Nairobi", "Nassau", "Naypyidaw", "New Delhi", "Ngerulmud"],
+                ["Niamey", "Nicosia", "Niger", "Nouakchott", "Nuku'alofa", "Oslo", "Ottawa"],
+                ["Ouagadougou", "Palikir", "Panama City", "Paramaribo", "Paris", "Phnom Penh", "Podgorica"],
+                ["Port-au-Prince", "Port Louis", "Port Moresby", "Port Vila", "Port of Spain", "Porto-Novo", "Prague"],
+                ["Praia", "Pretoria", "Pristina", "Pyongyang", "Quito", "Rabat", "Ramallah"],
+                ["Reykjavik", "Riga", "Riyadh", "Rome", "Roseau", "San Jose", "San Salvador"],
+                ["Sana'a", "Santiago", "Santo Domingo", "Sao Tome", "Sarajevo", "Seoul", "Singapore"],
+                ["Skopje", "Sofia", "South Tarawa", "Saint George's", "Saint John's", "Stockholm", "Sucre"],
+                ["Suva", "Taipei", "Tallinn", "Tashkent", "Tbilisi", "Tegucigalpa", "Tehran"],
+                ["Thimphu", "Tirana", "Tokyo", "Tripoli", "Tunis", "Ulaanbaatar", "Vaduz"],
+                ["Valletta", "Vatican City", "Vienna", "Victoria", "Vientiane", "Vilnius", "Warsaw"],
+                ["Washington D.C.", "Wellington", "Windhoek", "Yaounde", "Yamoussoukro", "Yaren", "Yerevan"],
+                ["Zagreb"]
             ],
 
             timer: 900
@@ -55,6 +91,17 @@ const QuizPage = () => {
                 ["United Kingdom", "Vatican City"]
             ],
 
+            capitals: [
+                ["Amsterdam", "Andorra la Vella", "Ankara", "Astana", "Athens", "Baku", "Belgrade"],
+                ["Berlin", "Bern", "Bratislava", "Brussels", "Bucharest", "Budapest", "Chisinau"],
+                ["Copenhagen", "Dublin", "Helsinki", "Kyiv", "Lisbon", "Ljubljana", "London"],
+                ["Luxembourg", "Madrid", "Minsk", "Monaco", "Moscow", "Nicosia", "Oslo"],
+                ["Paris", "Podgorica", "Prague", "Pristina", "Reykjavik", "Riga", "Rome"],
+                ["San Marino", "Sarajevo", "Skopje", "Sofia", "Stockholm", "Tbilisi", "Tallinn"],
+                ["Tirana", "Vaduz", "Valletta", "Vatican City", "Vienna", "Vilnius", "Warsaw"],
+                ["Yerevan", "Zagreb"]
+            ],
+
             timer: 300
         },
 
@@ -66,8 +113,19 @@ const QuizPage = () => {
                 ["Kyrgyzstan", "Laos", "Lebanon", "Malaysia", "Maldives", "Mongolia", "Myanmar"],
                 ["Nepal", "North Korea", "Oman", "Pakistan", "Palestine", "Philippines", "Qatar"],
                 ["Russia", "Saudi Arabia", "Singapore", "South Korea", "Sri Lanka", "Syria", "Tajikistan"],
-                ["Thailand", "Timor-Leste", "Turkiye", "Turkmenistan", "United Arab Emirates", "Uzbekistan", "Vietnam"],
-                ["Yemen"]
+                ["Thailand", "Taiwan", "Timor-Leste", "Turkiye", "Turkmenistan", "United Arab Emirates", "Uzbekistan"],
+                ["Vietnam", "Yemen"]
+            ],
+
+            capitals: [
+                ["Abu Dhabi", "Amman", "Ankara", "Ashgabat", "Astana", "Baghdad", "Baku"],
+                ["Bandar Seri Begawan", "Bangkok", "Beijing", "Beirut", "Bishkek", "Phnom Penh", "Cairo"],
+                ["Colombo", "Damascus", "Dhaka", "Dili", "Doha", "Dushanbe", "Hanoi"],
+                ["Islamabad", "Jakarta", "Jerusalem", "Kabul", "Kathmandu", "Kuala Lumpur", "Kuwait City"],
+                ["Male", "Manama", "Manila", "Moscow", "Muscat", "Naypyidaw", "New Delhi"],
+                ["Nicosia", "Pyongyang", "Ramallah", "Riyadh", "Sana'a", "Seoul", "Singapore"],
+                ["Taipei", "Tashkent", "Tbilisi", "Tehran", "Thimphu", "Tokyo", "Ulaanbaatar"],
+                ["Vientiane", "Yerevan"]
             ],
 
             timer: 300
@@ -85,6 +143,17 @@ const QuizPage = () => {
                 ["Togo", "Tunisia", "Uganda", "Zambia", "Zimbabwe"]
             ],
 
+            capitals: [
+                ["Abuja", "Accra", "Addis Ababa", "Algiers", "Antananarivo", "Asmara", "Bamako"],
+                ["Bangui", "Banjul", "Bissau", "Brazzaville", "Cairo", "Conakry", "Dakar"],
+                ["Djibouti", "Dodoma", "Freetown", "Gaborone", "Gitega", "Harare", "Juba"],
+                ["Kampala", "Khartoum", "Kigali", "Kinshasa", "Libreville", "Lilongwe", "Lome"],
+                ["Luanda", "Lusaka", "Malabo", "Maputo", "Maseru", "Mbabane", "Mogadishu"],
+                ["Monrovia", "Moroni", "Nairobi", "N'Djamena", "Niamey", "Nouakchott", "Ouagadougou"],
+                ["Port Louis", "Porto-Novo", "Praia", "Pretoria", "Rabat", "Sao Tome", "Tripoli"],
+                ["Tunis", "Victoria", "Windhoek", "Yamoussoukro", "Yaounde"]
+            ],
+
             timer: 360
         },
 
@@ -97,6 +166,14 @@ const QuizPage = () => {
                 ["Saint Vincent and the Grenadines", "Trinidad and Tobago", "United States of America"]
             ],
 
+            capitals: [
+                ["Basseterre", "Belmopan", "Bridgetown", "Castries", "Guatemala City"],
+                ["Havana", "Kingston", "Kingstown", "Managua", "Mexico City"],
+                ["Nassau", "Ottawa", "Panama City", "Port-au-Prince", "Port of Spain"],
+                ["Roseau", "San Jose", "San Salvador", "Santo Domingo", "Saint George's"],
+                ["Saint John's", "Tegucigalpa", "Washington D.C."]
+            ],
+
             timer: 300
         },
 
@@ -105,6 +182,12 @@ const QuizPage = () => {
                 ["Argentina", "Bolivia", "Brazil", "Chile"],
                 ["Columbia", "Ecuador", "Guyana", "Paraguay"],
                 ["Peru", "Suriname", "Uruguay", "Venezuela"],
+            ],
+
+            capitals: [
+                ["Asuncion", "Bogota", "Brasilia", "Buenos Aires"],
+                ["Caracas", "Georgetown", "Lima", "Montevideo"],
+                ["Paramaribo", "Quito", "Santiago", "Sucre"]
             ],
 
             timer: 240
@@ -117,6 +200,12 @@ const QuizPage = () => {
                 ["Soloman Islands", "Tonga", "Tuvalu", "Vanuatu"]
             ],
 
+            capitals: [
+                ["Apia", "Canberra", "Funafuti", "Honiara", "Majuro"],
+                ["Ngerulmud", "Nuku'alofa", "Palikir", "Port Moresby", "Port Vila"],
+                ["South Tarawa", "Suva", "Wellington", "Yaren"],
+            ],
+            
             timer: 240
         }
     };
@@ -129,7 +218,7 @@ const QuizPage = () => {
             const timeout = setTimeout(() => setTimer(timer - 1), 1000);
             return () => clearTimeout(timeout);
         } else {
-            setShowModal(true);
+            setShowModal2(true);
         }
     }, [timer]);
 
@@ -146,13 +235,13 @@ const QuizPage = () => {
         }
 
         if (Object.keys(correctCountries).length === countries.flat().length) {
-            setShowModal(true);
+            setShowModal2(true);
         }
     };
 
     const closeModal = () => {
-        setShowModal(false);
-        setTimer(60);
+        setShowModal2(false);
+        setTimer(continentQuizzes[continent].timer);
         setCorrectCountries({});
     };
 
@@ -191,7 +280,22 @@ const QuizPage = () => {
                 ))}
             </Container>
 
-            <Modal show={showModal} onHide={closeModal}>
+            <Modal show={showModal1} onHide={handleClose} backdrop="static">
+                <Modal.Header closeButton>
+                    <Modal.Title>Welcome to my website!</Modal.Title>
+                </Modal.Header>
+                    <Modal.Body>
+                        <p>This is a demo modal that opens when the page loads.</p>
+                        <p>You can customize it to show any content you want.</p>
+                    </Modal.Body>
+                <Modal.Footer>
+                    <Button variant="secondary" onClick={handleClose}>
+                        Close
+                    </Button>
+                </Modal.Footer>
+            </Modal>
+
+            <Modal show={showModal2} onHide={closeModal}>
                 <Modal.Header closeButton>
                     <Modal.Title>Quiz Results</Modal.Title>
                 </Modal.Header>
