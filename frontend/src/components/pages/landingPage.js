4e70b2535d5a4c017933c656600bfda5ca794d36
bchampagneATSSU <b_champagne@salemstate.edu>
2023-05-08 11:42:05 -0400

    Updated landingPage

--- frontend/src/components/pages/landingPage.js
+++ frontend/src/components/pages/landingPage.js
@@ -1,26 +1,25 @@
-import React, {} from 'react'
-import Card from 'react-bootstrap/Card';
+import React from 'react';
+import { Card, Button } from 'react-bootstrap';
 
 const Landingpage = () => {
   return (
-    <Card style={{ width: '30rem' }} className="mx-2 my-2">
-      <Card.Body>
-        <Card.Title>
-          GeograBee
-        </Card.Title>
-        <Card.Subtitle className="mb-2 text-muted">
-          Discover Our World
-        </Card.Subtitle>
-        <Card.Text></Card.Text>
-        <Card.Link href="/signup">
-          Register
-        </Card.Link>
-        <Card.Link href="/login">
-          Login
-        </Card.Link>
-      </Card.Body>
-    </Card>
-  )
-}
+    <div className="d-flex align-items-center justify-content-center" style={{ minHeight: '92.8vh', backgroundColor: '#00009a' }}>
+      <Card style={{ backgroundColor: '#009a00', width: '50%', height: '50vh', fontSize: '1.5rem' }} className="mx-2 my-2">
+        <Card.Body className="d-flex flex-column align-items-center justify-content-center" style={{ height: '100%' }}>
+          <Card.Title className="d-flex align-items-center justify-content-center" style={{ fontSize: '2.5rem' }}>
+            GeograBee
+          </Card.Title>
+          <Card.Subtitle className="mb-2 d-flex align-items-center justify-content-center" style={{ fontSize: '1.75rem' }}>
+            Discover Our World
+          </Card.Subtitle>
+          <div className="d-flex align-items-center justify-content-center mt-3">
+            <Button variant="light" href="/signup" className="mx-2" style={{ fontSize: '1.5rem', padding: '0.5rem 1rem' }}>Register</Button>
+            <Button variant="light" href="/login" className="mx-2" style={{ fontSize: '1.5rem', padding: '0.5rem 1rem' }}>Login</Button>
+          </div>
+        </Card.Body>
+      </Card>
+    </div>
+  );
+};
 
-export default Landingpage
+export default Landingpage;
